b78cb1e8ca12bf0e87b8f791b25a3a14cf12c0c3
ViktorKirin <113471321+ViktorKirin@users.noreply.github.com>
2022-09-13 21:53:01 +0200

    add converter

---
+++ code.js
@@ -0,0 +1,16 @@
+let curs = document.getElementById('curs');
+let mount = document.getElementById('mount');
+let button = document.querySelector('button');
+let out = document.querySelector('div');
+
+
+button.onclick = () => {
+    if (mount.value <= 0 || curs.value <= 0) {
+        out.innerHTML = `we can't do anything today`;
+        } else {
+    let summ = mount.value / curs.value;
+    out.innerHTML = `You can buy ${summ.toFixed(7)} BTC` ;
+}
+}
+
+
