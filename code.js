ead50757c071bb5e4192a24203eae675a152add7
ViktorKirin <113471321+ViktorKirin@users.noreply.github.com>
2022-09-13 23:59:14 +0200

    do better

--- code.js
+++ code.js
@@ -1,16 +1,17 @@
 let curs = document.getElementById('curs');
 let mount = document.getElementById('mount');
 let button = document.querySelector('button');
-let out = document.querySelector('div');
+let out = document.querySelector('.summ');
 
 
 button.onclick = () => {
+    
     if (mount.value <= 0 || curs.value <= 0) {
         out.innerHTML = `we can't do anything today`;
         } else {
     let summ = mount.value / curs.value;
     out.innerHTML = `You can buy ${summ.toFixed(7)} BTC` ;
-}
+    }
 }
 
 
